526a2ad1f05168bf718e9a7122dc0964fd935b08
Ian Wilson <iwilson@athlinllc.com>
2013-07-24 10:36:27 -0400

    Remove console.log

--- commands/whois.js
+++ commands/whois.js
@@ -2,7 +2,6 @@ exports.command = function (rooms, items, players, npcs, Commands)
 {
 	return function (args, player)
 	{
-        console.log(args);
 		players.eachIf(function(p){
             return p.getName() === args;
         }, function (p) {
